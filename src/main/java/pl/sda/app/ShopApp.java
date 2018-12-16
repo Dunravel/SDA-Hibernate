59b2f282e23d3fc79388447a69b7e66153e5ccbd
Marcin Iskra <dunravel@gmail.com>
2018-12-16 10:46:59 +0100

    Added PRoduct class and mapped it to hibernate

--- src/main/java/pl/sda/app/ShopApp.java
+++ src/main/java/pl/sda/app/ShopApp.java
@@ -1,4 +1,5 @@
 package pl.sda.app;
+import pl.sda.app.domain.Product;
 import pl.sda.app.hibernate.*;
 
 import org.hibernate.Session;
@@ -15,8 +16,16 @@ public class ShopApp
         // Jeżeli robimty tylko odczyt, to nie potrzebujemy Try Catch, jeżeli zmieniamy coś na bazie to lepiej jest zrobić.
         try {
             session.getTransaction().begin();
+
+            String catalogNumber = "910-000111";
+            String name = "laptop";
+            Product product = new Product(name, catalogNumber);
+            Integer productId = (Integer) session.save(product);
+            System.out.println(productId);
+
             session.getTransaction().commit();
         } catch(Exception e){
+            e.printStackTrace();
             session.getTransaction().rollback();
         }
         session.close();
---
+++ src/main/java/pl/sda/app/domain/Product.java
@@ -0,0 +1,31 @@
+package pl.sda.app.domain;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue
+    @Column(name = "product_id")
+    private int id;
+
+    private String name;
+    @Column(name = "catalog_number")
+    private String catalogNumber;
+
+    private String description;
+    private Timestamp updateDate;
+
+    public Product(String name, String catalogNumber) {
+        this.name = name;
+        this.catalogNumber = catalogNumber;
+        this.updateDate =   new Timestamp(System.currentTimeMillis());
+    }
+
+    private Product(){
+
+    }
+
+}
